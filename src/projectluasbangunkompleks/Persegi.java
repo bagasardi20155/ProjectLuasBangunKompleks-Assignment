49aab89485db6026395638751721a6621b441271
bagasardi20155 <bagasardi20155@gmail.com>
2021-10-06 22:24:37 +0700

    Project Upload

---
+++ src/projectluasbangunkompleks/Persegi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectluasbangunkompleks;
+
+/**
+ *
+ * @author Predator
+ */
+public class Persegi {
+    
+    static double hitungLuas(double s) {
+        
+        double luasPersegi = s * s;
+        return luasPersegi;
+    }
+}
---
+++ src/projectluasbangunkompleks/Project1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectluasbangunkompleks;
+
+/**
+ *
+ * @author Predator
+ */
+public class Project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double lingkaran = Lingkaran.hitungLuas(21);
+        double persegi = Persegi.hitungLuas(42);
+        
+        double LuasTotal = (lingkaran * 2) + persegi;
+        
+        System.out.println("Luas Bangun Datar Tersbut adalah : " + LuasTotal);
+    }
+    
+}
